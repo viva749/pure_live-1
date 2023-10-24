703e9b79cc389fca61dd6b61c254c6d419412898
liuchuancong <17792321552@163.com>
2023-10-25 01:01:53 +0800

    fix(EasyRefresh)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:dynamic_color/dynamic_color.dart';
+import 'package:easy_refresh/easy_refresh.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get.dart';
@@ -22,7 +23,26 @@ void main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
   JsEngine.init();
   PrefUtil.prefs = await SharedPreferences.getInstance();
-  
+  EasyRefresh.defaultHeaderBuilder = () => const ClassicHeader(
+        armedText: '松开加载',
+        dragText: '上拉刷新',
+        readyText: '加载中...',
+        processingText: '正在刷新...',
+        noMoreText: '没有更多数据了',
+        failedText: '加载失败',
+        messageText: '上次加载时间 %T',
+        processedText: '加载成功',
+      );
+  EasyRefresh.defaultFooterBuilder = () => const ClassicFooter(
+        armedText: '松开加载',
+        dragText: '下拉刷新',
+        readyText: '加载中...',
+        processingText: '正在刷新...',
+        noMoreText: '没有更多数据了',
+        failedText: '加载失败',
+        messageText: '上次加载时间 %T',
+        processedText: '加载成功',
+      );
   if (Platform.isWindows) {
     MediaKit.ensureInitialized();
     await WindowsSingleInstance.ensureSingleInstance(
