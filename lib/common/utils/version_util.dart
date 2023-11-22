c2830fcd811288e4a4dd6ba0db4130c1be66f2fd
liuchuancong <17792321552@163.com>
2023-11-22 20:42:59 +0800

    update(version)

--- lib/common/utils/version_util.dart
+++ lib/common/utils/version_util.dart
@@ -2,19 +2,16 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class VersionUtil {
-  static const String version = '1.4.0';
+  static const String version = '1.4.1';
   static const String projectUrl = 'https://github.com/liuchuancong/pure_live';
-  static const String releaseUrl =
-      'https://api.github.com/repos/liuchuancong/pure_live/releases';
-  static const String issuesUrl =
-      'https://github.com/liuchuancong/pure_live/issues';
+  static const String releaseUrl = 'https://api.github.com/repos/liuchuancong/pure_live/releases';
+  static const String issuesUrl = 'https://github.com/liuchuancong/pure_live/issues';
   static const String kanbanUrl =
       'https://jackiu-notes.notion.site/50bc0d3d377445eea029c6e3d4195671?v=663125e639b047cea5e69d8264926b8b';
 
   static const String githubUrl = 'https://github.com/liuchuancong';
   static const String email = '17792321552@163.com';
-  static const String emailUrl =
-      'mailto:17792321552@163.com?subject=PureLive Feedback';
+  static const String emailUrl = 'mailto:17792321552@163.com?subject=PureLive Feedback';
   static const String telegramGroup = 't.me/pure_live_channel';
   static const String telegramGroupUrl = 'https://t.me/pure_live_channel';
 
