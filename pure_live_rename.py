b80084184bd7414e0ffe39bab4a1d7a0d9ab9333
liuchuancong <17792321552@163.com>
2023-12-03 13:06:55 +0800

    fix(build shell)

--- pure_live_rename.py
+++ pure_live_rename.py
@@ -2,50 +2,40 @@ import os
 import shutil
 import time
 source_dir_name = 'E:/project/pure_live_release/'
-target_apk_dir_name = 'E:/project/pure_live/build/windows/x64/runner/Release/'
-target_win_app_name = 'E:/project/pure_live/build/app/outputs/flutter-apk/'
+target_win_app_name  = 'E:/project/pure_live/build/windows/x64/runner/Release/'
+target_apk_dir_name = 'E:/project/pure_live/build/app/outputs/flutter-apk/'
+target_files = ['app-arm64-v8a-release.apk','app-armeabi-v7a-release.apk','app-release.apk','app-x86_64-release.apk','pure_live.msix']
+build_path = []
+buildcellctions = []
 # target_apk_dir_name = 'D:/flutter/pure_live/build/windows/x64/runner/Release/'
 # target_win_app_name = 'D:/flutter/pure_live/build/app/outputs/flutter-apk/'
 files = []
-apkDirArr = ['all', 'v7a', 'v8a', 'x86']
-allowedFileArr = ['apk', 'msix']
-windowsName = 'pure_live'
-def traversal_dirs(path,version):
-    for file in os.listdir(path):
-        file_path = os.path.join(path, file)
-        if os.path.isdir(file_path):
-           type = file_path.split('/')[-1].split('-')[-1]
-           isApk = type in apkDirArr
-           if isApk:
-              rename = version + '-' + type
-              os.rename(file_path, os.path.join(path, rename))
-           else:
-              rename = 'windows-' + version
-              os.rename(file_path, os.path.join(path, rename))
-          
+dirArr = ['安卓_手机高版本','安卓_手机低版本或电视','不知道就下载这个','模拟器','win10以上系统下载']
+def traversal_dirs(path):
+    for item in os.scandir(path):
+        if item.is_dir():
+            shutil.rmtree(item.path)
+        else:
+            os.remove(item.path)
+            
 def traversal_files(path):
     for item in os.scandir(path):
         if item.is_file():
             fileName = item.path.split('/')[-1].split('\\')[-1]
-            filePath = item.path.split('/')[-1].split('\\')[0]
-            type = item.path.split('/')[-1].split('\\')[-1].split('.')[1]
-            # 先删除zip文件
-            if fileName.split('.')[-1] == "zip":
-                os.remove(item.path)
-            allowed = type in allowedFileArr
-            # 复制apk
-            if allowed:
-              os.remove(item.path)
-              src = target_apk_dir_name + fileName
-              dst = source_dir_name + filePath
-              if os.path.exists(src):
-                  shutil.copy(src, dst)
-              else:
-                  # msix
-                  src = target_win_app_name + fileName
-                  shutil.copy(src, dst)
-        else:
-            traversal_files(item)
+            if fileName in target_files:
+                buildcellctions.append(item.path)
+    
+def traversal_target_files(path,version):
+    if (len(buildcellctions)!=len(dirArr)):
+      print('请全部打包')
+      return
+    for i in range(0,len(dirArr)):
+            src = os.path.join(path,version + '-' +dirArr[i])
+            os.mkdir(src,mode=0o777)
+            file = open(os.path.join(src,target_files[i]), 'w')
+            file.close()
+            source = buildcellctions[i]
+            shutil.copy(source, src)
 def zip_dirs(path):
     for file in os.listdir(path):
         file_path = os.path.join(path, file)
@@ -54,12 +44,18 @@ def zip_dirs(path):
 
 def main():
         version = str(input("请输入你想发布的版本："))
-        # 先改名字
-        traversal_dirs(source_dir_name,version)
+        # 先改名字12
+        traversal_dirs(source_dir_name)
+        
+        
         # 复制文件
-        traversal_files(source_dir_name)
+        
+        traversal_files(target_apk_dir_name)
+        traversal_files(target_win_app_name)
+        traversal_target_files(source_dir_name,version)
+        print(buildcellctions)
         # 压缩为zip
-        zip_dirs(source_dir_name)
+        # zip_dirs(source_dir_name)
         # 12
 if __name__ == '__main__':
      main()
